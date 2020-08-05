268d1fbaf04d6bfc5fd4a17099e18bdde0ea218b
Pluveto <receding@protonmail.com>
2020-08-06 06:42:07 +0800

    update

---
+++ linkedlist/deleteNode.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+template <class T>
+void printVec(vector<T> &vec);
+
+template <class T>
+void printMat(vector<vector<T>> &mat);
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        ListNode *tmp = node->next;
+        node->val = tmp->val;
+        node->next = tmp->next;
+        delete tmp;
+    }
+};
+
+template <class T>
+void printVec(vector<T> &vec)
+{
+    size_t size = vec.size();
+    cout << "{";
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << vec[i];
+        if (i != size - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << "}";
+}
+
+template <class T>
+void printMat(vector<vector<T>> &mat)
+{
+    size_t size = mat.size();
+    cout << "{";
+    for (size_t i = 0; i < size; i++)
+    {
+        printVec(mat[i]);
+        if (i != size - 1)
+        {
+            cout << ", \n";
+        }
+    }
+    cout << "}";
+}
+
+int main()
+{
+    Solution *sol = new Solution();
+    return 0;
+}
